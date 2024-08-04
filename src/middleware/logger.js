8c3f370f8c83ddb270e1d2df8002626b7c5afee0
Emir Colakbuyuk <emircolakbuyuk@sabanciuniv.edu>
2024-08-04 15:36:48 +0300

    logs handled

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -10,8 +10,27 @@ if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
 
 const logger = (req, res, next) => {
   const username = req.user ? req.user.username : 'Anonymous';
-  const log = `${new Date().toISOString()} - ${req.method} ${req.originalUrl} - User: ${username}\n`;
-  fs.appendFileSync(logFilePath, log);
+
+  // Capture the original send function
+  const originalSend = res.send;
+
+  // Wrap the send function to log the response
+  res.send = function (body) {
+    // Log the primary line
+    const logPrimaryLine = `${new Date().toISOString()} - ${req.method} ${req.originalUrl} - User: ${username}\n`;
+
+    // Log the JSON object with request body and response
+    const logDetails = {
+      requestBody: req.body,
+      responseBody: body
+    };
+    
+    fs.appendFileSync(logFilePath, logPrimaryLine);
+    fs.appendFileSync(logFilePath, JSON.stringify(logDetails, null, 2) + '\n');
+    
+    originalSend.call(this, body);
+  };
+
   next();
 };
 
